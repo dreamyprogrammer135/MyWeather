bdbf1428e26c86953c52f8906eaad99b54b38f69
Litvinov Ilya <dreameprogrammer82@gmail.com>
2021-07-29 10:57:50 +0300

    Homework_6 Broadcast Receiver

--- app/src/main/java/com/dreamyprogrammer/myweather/view/MainActivity.kt
+++ app/src/main/java/com/dreamyprogrammer/myweather/view/MainActivity.kt
@@ -1,5 +1,12 @@
 package com.dreamyprogrammer.myweather.view
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.BroadcastReceiver
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.ConnectivityManager
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.dreamyprogrammer.myweather.R
@@ -8,6 +15,7 @@ import com.dreamyprogrammer.myweather.databinding.MainActivityBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: MainActivityBinding
+    private val networkStatusReceiver: BroadcastReceiver = NetworkStatus()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,5 +30,30 @@ class MainActivity : AppCompatActivity() {
                 .replace(R.id.container, NavigationContainerFragment())
                 .commitNow()
         }
+
+        setBroadcastMessages()
+    }
+
+    private fun setBroadcastMessages() {
+//        registerReceiver(networkStatusReceiver, IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED))
+        //       registerReceiver(networkStatusReceiver, new IntentFilter(Intent.ACTION_POWER_CONNECTED));
+        registerReceiver(networkStatusReceiver, IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
+        initNotificationChanne()
+    }
+
+
+    // инициализация канала нотификаций
+    private fun initNotificationChanne() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            val importance = NotificationManager.IMPORTANCE_LOW
+            val channel = NotificationChannel("2", "name", importance)
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    override fun onDestroy() {
+        unregisterReceiver(networkStatusReceiver)
+        super.onDestroy()
     }
 }
